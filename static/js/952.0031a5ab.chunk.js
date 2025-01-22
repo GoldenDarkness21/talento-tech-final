ecfca01b2157b02ccc7e4d366cfe506d2342e331
Brandon <martinezbran0221@gmail.com>
2025-01-22 17:49:18 -0500

    Updates

---
+++ static/js/952.0031a5ab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_web=self.webpackChunkreact_web||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.0031a5ab.chunk.js.map
